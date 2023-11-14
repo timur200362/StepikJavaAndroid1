ebb18bd8031bbe1f8b169543dacc22887c0dbd63
timur200362 <kh-g-2016@mail.ru>
2023-11-14 22:46:43 +0300

    MVVM(7.16 lesson)

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         });
         initViews();
 
+
         notesAdapter = new NotesAdapter();
         notesAdapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
             @Override
