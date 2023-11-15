0bae5d85b16a3fe78674cc93ea91711ca8f97ab7
timur200362 <kh-g-2016@mail.ru>
2023-11-15 15:15:32 +0300

    disposable(8.3 lesson)

--- app/src/main/java/com/example/todolist/AddNoteViewModel.java
+++ app/src/main/java/com/example/todolist/AddNoteViewModel.java
@@ -9,22 +9,25 @@ import androidx.lifecycle.MutableLiveData;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class AddNoteViewModel extends AndroidViewModel {
     private NotesDao notesDao;
     private MutableLiveData<Boolean> shouldCloseScreen=new MutableLiveData<>();
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();//коллекция всех подписок
 
     public AddNoteViewModel(@NonNull Application application){
         super(application);
-        notesDao=NoteDatabase.getInstance(application).notesDao();
+        notesDao = NoteDatabase.getInstance(application).notesDao();
     }
     public LiveData<Boolean> getShouldCloseScreen(){
         return shouldCloseScreen;
     }
     public void saveNote(Note note){
-        notesDao.add(note)
+        Disposable disposable = notesDao.add(note)
                 .subscribeOn(Schedulers.io())//переводим(верхний код) добавление в фоновый поток. io-работа с данными
                 .observeOn(AndroidSchedulers.mainThread())//переводим(нижний код) в главный поток.
                 .subscribe(new Action() {
@@ -33,5 +36,12 @@ public class AddNoteViewModel extends AndroidViewModel {
                 shouldCloseScreen.setValue(true);//setValue-только главный поток
             }
         });
+        compositeDisposable.add(disposable);//добавляем объект disposable для добавления записи в коллекцию
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();//отменяем подписку, если viewModel уничтожится
     }
 }
